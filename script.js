21fe76240014663d2d6c06706457dfdf9aa17f78
jeba keshiya <jebakeshiyad@gmail.com>
2025-09-19 15:17:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("h1")[0].style.fontSize = "6vw";function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  if (section) {
+    section.scrollInto
